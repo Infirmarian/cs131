a7d1c60210666b0b76784db54cae2c4b2479b096
Robert Geil <robert@rgeil.net>
2019-01-15 15:01:13 -0800

    Completed set_diff and computed_fixed_point and associated tests

--- hw1.ml
+++ hw1.ml
@@ -1,13 +1,7 @@
-
-(* Function returns true if value a is in list b, and false otherwise 
- * This is a helper function for subset *)
-let rec lcontains a b = 
-  if List.length b = 0 then false 
-  else if List.hd b = a then true else lcontains a (List.tl b);;
 (*Returns true iff a is a subset of b, and false otherwise*)
 let rec subset a b = 
   if List.length a = 0 then true 
-  else if lcontains (List.hd a) b then subset (List.tl a) b 
+  else if List.exists (fun x -> x=(List.hd a)) b then subset (List.tl a) b 
       else false;;
 (* Returns true if two SORTED, UNIQUE lists are equivelant*)
 let rec e_s_u a b = 
@@ -22,8 +16,18 @@ let equal_sets a b =
 let set_union a b =
   List.append a b;;
 
+(* Give the intersection of two lists *)
 let rec set_intersection a b = 
   if List.length a = 0 then [] 
   else if List.exists (fun x -> x=(List.hd a)) b then List.cons (List.hd a) (set_intersection (List.tl a) b)
         else set_intersection (List.tl a) b;;
+
+let rec set_diff a b = 
+  if List.length a = 0 then []
+  else if List.exists (fun x -> x=(List.hd a)) b then set_diff (List.tl a) b 
+    else List.cons (List.hd a) (set_diff (List.tl a) b);;
+
+let rec computed_fixed_point eq f x = 
+  if eq (f x) x then x else computed_fixed_point eq f (f x);;
+
 
--- hw1test.ml
+++ hw1test.ml
@@ -1,49 +1,67 @@
-let subset_0 = subset [] []
-let subset_1 = subset [] [1]
-let subset_2 = subset [1] [1]
-let subset_3 = subset [1;2] [1;2]
-let subset_4 = subset [1;2] [1;2;3]
-let subset_5 = subset [2;1] [1;2]
-let subset_6 = subset [1;1;1;1] [1;2]
-let subset_7 = subset ["a"] ["a"]
-let subset_8 = subset [1.3; 1.4;] [1.3; 1.9; 1.4]
+let my_subset_0 = subset [] []
+let my_subset_1 = subset [] [1]
+let my_subset_2 = subset [1] [1]
+let my_subset_3 = subset [1;2] [1;2]
+let my_subset_4 = subset [1;2] [1;2;3]
+let my_subset_5 = subset [2;1] [1;2]
+let my_subset_6 = subset [1;1;1;1] [1;2]
+let my_subset_7 = subset ["a"] ["a"]
+let my_subset_8 = subset [1.3; 1.4;] [1.3; 1.9; 1.4]
 
 (*Non subsets*)
-let subset_9 = not (subset [1] [])
-let subset_10 = not (subset [1;2] [1])
-let subset_11 = not (subset [1;2;3] [1;2;7])
-let subset_12 = not (subset [9;3] [9;2;7])
+let my_subset_9 = not (subset [1] [])
+let my_subset_10 = not (subset [1;2] [1])
+let my_subset_11 = not (subset [1;2;3] [1;2;7])
+let my_subset_12 = not (subset [9;3] [9;2;7])
 
 
 (* Equal sets *)
-let eq_set_test0 = equal_sets [] []
-let eq_set_test1 = equal_sets [1] [1]
-let eq_set_test2 = equal_sets [1;1;1] [1]
-let eq_set_test3 = equal_sets [1;2;3] [1;2;3]
-let eq_set_test4 = equal_sets [1;2;3;5;5;5] [5;3;2;1]
-let eq_set_test5 = equal_sets [9;9;9;1] [9;1;1;1]
+let my_equal_sets_test0 = equal_sets [] []
+let my_equal_sets_test1 = equal_sets [1] [1]
+let my_equal_sets_test2 = equal_sets [1;1;1] [1]
+let my_equal_sets_test3 = equal_sets [1;2;3] [1;2;3]
+let my_equal_sets_test4 = equal_sets [1;2;3;5;5;5] [5;3;2;1]
+let my_equal_sets_test5 = equal_sets [9;9;9;1] [9;1;1;1]
 (* Not equal sets *)
-let eq_set_test6 = not (equal_sets [] [1])
-let eq_set_test7 = not (equal_sets [1] [])
-let eq_set_test8 = not (equal_sets [1] [2])
-let eq_set_test9 = not (equal_sets [1;2;3] [2;3])
-let eq_set_test10 = not (equal_sets [1;2;3] [2;3;4])
+let my_equal_sets_test6 = not (equal_sets [] [1])
+let my_equal_sets_test7 = not (equal_sets [1] [])
+let my_equal_sets_test8 = not (equal_sets [1] [2])
+let my_equal_sets_test9 = not (equal_sets [1;2;3] [2;3])
+let my_equal_sets_test10 = not (equal_sets [1;2;3] [2;3;4])
 
 (* Union of two sets *)
-let union_test0 = equal_sets (set_union [] []) []
-let union_test1 = equal_sets (set_union [1] []) [1]
-let union_test2 = equal_sets (set_union [] [1]) [1]
-let union_test3 = equal_sets (set_union [1] [1]) [1]
-let union_test4 = equal_sets (set_union [1] [2]) [1;2]
-let union_test5 = equal_sets (set_union [1] [1;2]) [1;2]
-let union_test6 = equal_sets (set_union [9;3] [1;2]) [1;2;3;9]
+let my_set_union_test0 = equal_sets (set_union [] []) []
+let my_set_union_test1 = equal_sets (set_union [1] []) [1]
+let my_set_union_test2 = equal_sets (set_union [] [1]) [1]
+let my_set_union_test3 = equal_sets (set_union [1] [1]) [1]
+let my_set_union_test4 = equal_sets (set_union [1] [2]) [1;2]
+let my_set_union_test5 = equal_sets (set_union [1] [1;2]) [1;2]
+let my_set_union_test6 = equal_sets (set_union [9;3] [1;2]) [1;2;3;9]
 
 (* Intersection of two sets *)
-let intersect_test0 = equal_sets (set_intersection [] []) []
-let intersect_test1 = equal_sets (set_intersection [1] []) []
-let intersect_test2 = equal_sets (set_intersection [1] [3]) []
-let intersect_test3 = equal_sets (set_intersection [1] [3;1]) [1]
-let intersect_test4 = equal_sets (set_intersection [1;1;5] [3;1]) [1]
-let intersect_test5 = equal_sets (set_intersection [1;2;3] [3;1]) [1;3]
-let intersect_test6= equal_sets (set_intersection [1;2;3;9] [9;3;2;1]) [1;2;3;9]
+let my_set_intersection_test0 = equal_sets (set_intersection [] []) []
+let my_set_intersection_test1 = equal_sets (set_intersection [1] []) []
+let my_set_intersection_test2 = equal_sets (set_intersection [1] [3]) []
+let my_set_intersection_test3 = equal_sets (set_intersection [1] [3;1]) [1]
+let my_set_intersection_test4 = equal_sets (set_intersection [1;1;5] [3;1]) [1]
+let my_set_intersection_test5 = equal_sets (set_intersection [1;2;3] [3;1]) [1;3]
+let my_set_intersection_test6 = equal_sets (set_intersection [1;2;3;9] [9;3;2;1]) [1;2;3;9]
 
+(* Difference of two sets (a-b) *)
+let my_set_diff_test0 = equal_sets (set_diff [] []) []
+let my_set_diff_test1 = equal_sets (set_diff [] [1]) []
+let my_set_diff_test2 = equal_sets (set_diff [1] [1]) []
+let my_set_diff_test3 = equal_sets (set_diff [1] [1;1]) []
+let my_set_diff_test4 = equal_sets (set_diff [1;1;1] [1]) []
+let my_set_diff_test5 = equal_sets (set_diff [1] []) [1]
+let my_set_diff_test6 = equal_sets (set_diff [1] [3]) [1]
+let my_set_diff_test7 = equal_sets (set_diff [1;2] [3]) [1;2]
+let my_set_diff_test8 = equal_sets (set_diff [1;2;3] [3]) [1;2]
+let my_set_diff_test9 = equal_sets (set_diff [1;2;3] [1;2;3]) []
+let my_set_diff_test10 = equal_sets (set_diff [4;3;2;1] [1;2;3;9]) [4]
+
+(* Fixed points of a function *)
+let my_computed_fixed_point_test0 = computed_fixed_point (=) (fun x -> x) 10 = 10
+let my_computed_fixed_point_test1 = computed_fixed_point (=) (fun x -> x/2) 1000 = 0
+let my_computed_fixed_point_test2 = computed_fixed_point (=) (fun x -> x *. 2.) 1000. = infinity
+let my_computed_fixed_point_test3 = computed_fixed_point (=) sqrt 9999. = 1.
