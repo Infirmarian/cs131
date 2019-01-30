fe9fe7d5b7c3cbeb79cfbad1d5ea274c79d409ff
Robert Geil <robert@rgeil.net>
2019-01-30 11:11:18 -0800

    Added basic function of make_matcher

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -31,7 +31,35 @@ let rec parse_tree_leaves tree =
                                             | Node (no,br) -> (parse_tree_leaves (Node(no,br)))@(helper t)) in helper c
 
 
-let make_matcher x =
-  x
+let rec mmm gram frag0 = 
+match gram with root,rules -> 
+	match (rules root) with 
+	| [] -> false
+	| h::t -> (match h with 
+		| [] -> false 
+		| r1::rn -> let rec matchboi hd tl = 
+			(match hd with 
+			| N non -> mmm (non,rules) frag0
+			| T term -> (term = frag0) || (match tl with [] -> false | ra::rb -> matchboi ra rb)) in matchboi r1 rn);;
+
+
+let rec make_matcher gram frag = 
+	match gram with e,f -> let tail = match_sub f (f e) frag in tail
+
+and match_sub rules li frag = 
+	match li with a::b -> let cv = match_list rules a frag in (match cv with | None -> match_sub rules b frag | Some x -> Some x)
+				| [] -> None
+
+and match_list rules list frag =
+	match frag with 
+		| first::last -> (
+		match list with | h::t -> (match h with | T term -> if term = first then match_list rules t last else None
+												| N non -> let finished = make_matcher (non,rules) frag in (match finished with None -> None | Some x -> match_list rules t x))
+						| [] -> Some frag)
+		| [] -> match list with [] -> Some[] | _ -> None
+
+
+
+
 let make_parser x =
   x
--- hw2/hw2test.ml
+++ hw2/hw2test.ml
@@ -6,3 +6,4 @@ let my_parse_tree_leaves_test2 = parse_tree_leaves (Node (".", [Leaf "1"])) = ["
 let my_parse_tree_leaves_test3 = parse_tree_leaves (Node (".", [Node (".", [Leaf "9"])])) = ["9"]
 let my_parse_tree_leaves_test4 = parse_tree_leaves (Node (".", [Node (".", [Leaf "9";Leaf "8"; Leaf "7"]); Leaf "1"])) = ["9"; "8"; "7"; "1"]
 let my_parse_tree_leaves_test5 = parse_tree_leaves (Node (".", [Node ("-", [Node ("*", [Node ("+", [Leaf 1]); Leaf 2]); Leaf 3]); Leaf 4])) = [1; 2; 3; 4]
+
