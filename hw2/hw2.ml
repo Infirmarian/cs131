a3e1725733838e60e8e5628f98401d6abd29d898
Robert Geil <robert@rgeil.net>
2019-01-31 15:09:44 -0800

    Completed make_parser

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -6,6 +6,47 @@ type ('nonterminal, 'terminal) parse_tree =
   | Node of 'nonterminal * ('nonterminal, 'terminal) parse_tree list
   | Leaf of 'terminal
 
+let accept_all string = Some string
+let accept_empty_suffix = function
+   | _::_ -> None
+   | x -> Some x
+
+
+
+(* An example grammar for a small subset of Awk.
+   This grammar is not the same as Homework 1; it is
+   instead the same as the grammar under
+   "Theoretical background" above.  *)
+
+type awksub_nonterminals =
+  | Expr | Term | Lvalue | Incrop | Binop | Num
+
+let awkish_grammar =
+  (Expr,
+   function
+     | Expr ->
+         [[N Term; N Binop; N Expr];
+          [N Term]]
+     | Term ->
+	 [[N Num];
+	  [N Lvalue];
+	  [N Incrop; N Lvalue];
+	  [N Lvalue; N Incrop];
+	  [T"("; N Expr; T")"]]
+     | Lvalue ->
+	 [[T"$"; N Expr]]
+     | Incrop ->
+	 [[T"++"];
+	  [T"--"]]
+     | Binop ->
+	 [[T"+"];
+	  [T"-"]]
+     | Num ->
+	 [[T"0"]; [T"1"]; [T"2"]; [T"3"]; [T"4"];
+	  [T"5"]; [T"6"]; [T"7"]; [T"8"]; [T"9"]])
+
+
+
 (* Given a type (expr) and rules (gram) this function returns a list of the grammars *)
 let rec convert_grammar_compile_type expr gram = 
   match gram with 
@@ -50,20 +91,26 @@ and match_list rules list acc frag =
 
 
 
-
-let rec make_parser gram frag = 
-	match gram with e,f -> let res = parse_opts f (f e) frag in match res with None -> None | Some x -> Some (Node (e,x))
+let rec make_runner gram frag = 
+	match gram with e,f -> let res = parse_opts f (f e) frag in match res with None -> None | Some (x,y) -> Some ((Node (e,x)),y)
 
 and parse_opts rules li frag = 
 	match li with [] -> None
-			| a::b -> let cv = match_list rules a frag in (match cv with None -> parse_opts rules b frag | Some x -> Some x)
+			| a::b -> let cv = match_final rules a frag in (match cv with None -> parse_opts rules b frag | Some (x,tail) -> (match tail with [] -> Some (x,tail) | _ -> (match parse_opts rules b frag with None -> Some(x,tail) | _ -> parse_opts rules b frag)))
 
-and match_list rules list frag = 
+and match_final rules list frag = 
 	match frag with 
-		| first::last -> ( match list with h::t -> (match h with | T term -> if term = first then let va = (match_list rules t last) in match va with | None -> None | Some x -> Some ((Leaf term)::x) else None
-																| N node -> let finish = make_parser (node,rules) frag in (match finish with None -> None | Some x -> match_list rules t last)
+		| first::last -> (match list with h::t -> (match h with | T term -> if term = first then let va = (match_final rules t last) in match va with | None -> None | Some (x,y) -> Some (((Leaf term)::x),y) else None
+																| N node -> let finish = make_runner (node,rules) frag in (match finish with None -> None | Some (it,y) -> let next = match_final rules t y in (match next with None -> None | Some (p,q) -> Some ((it::p),q)))
 															)
-										| [] -> None
+										| [] -> Some ([],frag)
 									)
-		| [] -> (match list with [] -> Some [] | _ -> None )
+		| [] -> (match list with [] -> Some ([],[]) | _ -> None )
 
+;;
+
+let make_parser gram frag =
+let res = make_runner gram frag in
+match res with
+| None -> None
+| Some (x,y) -> (match y with [] -> Some x | _ -> None)
