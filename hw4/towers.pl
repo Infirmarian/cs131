37f63632f4ff33089988ec4e9dcde184968c388f
Robert Geil <robert@rgeil.net>
2019-02-26 12:55:54 -0800

    Implemented an inefficient plain_tower

--- hw4/towers.pl
+++ hw4/towers.pl
@@ -1,64 +1,54 @@
 
-%base (trivial) case for a tower
 tower(0, [], counts([],[],[],[])) :- !.
-tower(N,[TH|TT],counts(Top, Bottom, Left, Right)) :- 
-    getAllValidCountsLeftRight([TH|TT], Left),
-    reverseAll([TH|TT], BoardReversed),
+tower(N, T, counts(Top, Bottom, Left, Right)) :- true.
+
+
+%base (trivial) case for a tower
+plain_tower(0, [], counts([],[],[],[])) :- !.
+plain_tower(N,T,counts(Top, Bottom, Left, Right)) :- 
+    generateAndLengthCheck(T, N, N),
+    transpose(T, BoardTranspose, N),
+    generateAndLengthCheck(BoardTranspose, N, N),
+    getAllValidCountsLeftRight(T, Left),
+    reverseAll(T, BoardReversed),
     getAllValidCountsLeftRight(BoardReversed, Right),
-    transpose([TH|TT], BoardTranspose, N),
     getAllValidCountsLeftRight(BoardTranspose, Top),
     reverseAll(BoardTranspose, BoardReverseTranspose),
-    getAllValidCountsLeftRight(BoardReverseTranspose, Bottom),
-    validTowerRows([TH|TT], N),
-    validTowerRows(BoardTranspose, N).
+    getAllValidCountsLeftRight(BoardReverseTranspose, Bottom).
 
 reverseAll([],[]).
-reverseAll([TH|TT],Result) :- reverse(TH, L1), reverseAll(TT, R2), append([L1], R2, Result).
-
+reverseAll([TH|TT],Result) :- reverse(TH, L1), reverseAll(TT, R2), append([L1], R2, Result), !.
 transpose(_, [], 0).
-transpose(Matrix, Result, N) :- transposeRow(Matrix, N, RowIndividual), NN is N - 1, transpose(Matrix, R2, NN), append(R2, [RowIndividual], Result).
-
+transpose(Matrix, Result, N) :- transposeRow(Matrix, N, RowIndividual), NN is N - 1, transpose(Matrix, R2, NN), append(R2, [RowIndividual], Result), !.
 transposeRow([], _, []).
-transposeRow([H|T], N, Result) :- transposeRow(T, N, R2), nth(N,H,A), append([A], R2, Result).
+transposeRow([H|T], N, Result) :- transposeRow(T, N, R2), nth(N,H,A), append([A], R2, Result), !.
 
 
 getAllValidCountsLeftRight([], []).
 getAllValidCountsLeftRight([[1]], [1]) :- !.
-getAllValidCountsLeftRight([H|T], [CountH|CountT]) :- validCountTowersVisible(H, CountH, 0), getAllValidCountsLeftRight(T, CountT).
+getAllValidCountsLeftRight([H|T], [CountH|CountT]) :- countTowersVisible(H, CountH, 0), getAllValidCountsLeftRight(T, CountT).
 
 
-
-
-% Determine if a given row fits the constraints of the tower problem
-% (Length N, max value N, min value 1, add elements different)
-validTowerRow([H], N) :- N #= 1, !.
-validTowerRow([H|T], N) :-  fd_domain(FDList, [H|T]), 
-                            fd_all_different([H|T]), 
-                            X #= 1, 
-                            Y #= N, 
-                            fd_max(FDList, Y), 
-                            fd_min(FDList, X).
-
-validTowerRows([], N).
-validTowerRows([H|T], N) :- validTowerRow(H, N), validTowerRows(T,N).
 
 % Determine if a given row fits the constraints WITHOUT Finite Domain solver
-validTowerRowNFD([H|T], N) :-   max_list([H|T], N), 
-                                min_list([H|T], 1), 
-                                length([H|T], N), 
-                                sort([H|T], R), 
-                                length(R, N).
+generateAndLengthCheck([], 0, _) :- !.
+generateAndLengthCheck(L, Remain, N) :- NRemain is Remain - 1, 
+                                        generateAndLengthCheck(R2, NRemain, N),
+                                        oneThroughNList(N, BaseArray), permutation(BaseArray, T), 
+                                        append(R2, [T], L).
 
+validTowerRowsNFD([], _).
+validTowerRowsNFD([H|T], N) :- validTowerRowNFD(H, N), validTowerRowsNFD(T,N).
+
+validTowerRowNFD(T, N) :- oneThroughNList(N, BaseArray), permutation(BaseArray, T).
+
+oneThroughNList(0,[]):-!.
+oneThroughNList(N, Result) :- N1 is N - 1, oneThroughNList(N1, R2), append(R2, [N], Result).
 
 % Determine if there are a correct number of towers visible, eg count how many
 % are being not blocked by another, higher tower
-maxHeight(N1, N2, R) :- N1 #>= N2, R #= N1.
-maxHeight(N1, N2, R) :- N2 #>= N1, R #= N2.
-heightGT(N1, N2, R) :- N1 #>= N2, R #= 1.
-heightGT(N1, N2, R) :- N1 #=< N2, R #= 0.
 
-validCountTowersVisible([], 0, _).
-validCountTowersVisible([H|T], N, CurrentHeight) :- maxHeight(H, CurrentHeight, R), 
-                                                    validCountTowersVisible(T, N2, R), 
-                                                    heightGT(H, CurrentHeight, Diff),
-                                                    N #= Diff + N2,!.
+countTowersVisible([], 0, _):-!.
+countTowersVisible([H|T], N, Current) :- H =< Current, countTowersVisible(T, N2, Current), N = N2, !.
+countTowersVisible([H|T], N, Current) :- H > Current, countTowersVisible(T, N2, H), N is N2 + 1, !.
+
